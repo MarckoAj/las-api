1cc7a33147c17a9fc38ac1883563fe30c9918916
MarckoAj <96995466+MarckoAj@users.noreply.github.com>
2022-06-09 17:04:40 -0300

    configurando swaggerUI

--- src/config/customExpress.js
+++ src/config/customExpress.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const consign = require("consign");
 const bodyParser = require("body-parser");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("../swagger.json.json");
 
 const ENV = process.env.NODE_ENV;
 
 module.exports = () => {
   const app = express();
-
+  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use(bodyParser.json());
 
